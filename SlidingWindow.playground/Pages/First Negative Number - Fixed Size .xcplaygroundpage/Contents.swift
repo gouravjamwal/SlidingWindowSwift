e39877b4e1f2652b16e56d7fc66f51f8da42bf0e
Gourav Jamwal <gjamwal@vmware.com>
2022-08-07 21:18:06 +0530

    Updated Descriptions

--- SlidingWindow.playground/Pages/First Negative Number - Fixed Size .xcplaygroundpage/Contents.swift
+++ SlidingWindow.playground/Pages/First Negative Number - Fixed Size .xcplaygroundpage/Contents.swift
@@ -1,3 +1,47 @@
-import UIKit
+/*
+ Problem Description: https://practice.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1
 
-var greeting = "Hello, playground"
+ Given an array and a positive integer k, find the first negative integer for each and every window(contiguous subarray) of size k.
+
+ Example:
+
+ Input:
+ 2
+ 5
+ -8 2 3 -6 10
+ 2
+ 8
+ 12 -1 -7 8 -15 30 16 28
+ 3
+ */
+import Foundation
+
+func firstNegativeNumberInEveryWindow(_ array: [Int],_ k: Int) -> [Int] {
+    var resultArray = Array(repeating: 0, count: array.count - k + 1)
+    var windowArray = [Int]()
+    var windowStart = 0
+    
+    
+    for windowEnd in 0..<array.count {
+        if array[windowEnd] < 0 {
+            windowArray.append(array[windowEnd] )
+        }
+        
+        if windowEnd - windowStart + 1 == k {
+            if !windowArray.isEmpty {
+                let value = windowArray[0]
+                resultArray[windowStart] = value
+                
+                if value == array[windowStart] {
+                    windowArray.removeFirst()
+                }
+            } else {
+                resultArray[windowStart] = 0
+            }
+            windowStart += 1
+        }
+    }
+    return resultArray
+}
+
+print(firstNegativeNumberInEveryWindow([12, -1, -7, 8, -15, 30, 16, 28], 3))
--- SlidingWindow.playground/Pages/Maximum Sum SubArray of Size K - Fixed Size .xcplaygroundpage/Contents.swift
+++ SlidingWindow.playground/Pages/Maximum Sum SubArray of Size K - Fixed Size .xcplaygroundpage/Contents.swift
@@ -1,29 +1,37 @@
+/*
+ Problem Description: https://practice.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1
 
+ Given an array of integers Arr of size N and a number K. Return the maximum sum of a subarray of size K.
+
+ Example:
+
+ Input:
+ N = 4, K = 2
+ Arr = [100, 200, 300, 400]
+ Output:
+ 700
+ Explanation:
+ Arr3  + Arr4 =700,
+ which is maximum. .
+ */
 import Foundation
 
-class MaximumSumSubArrayOfSizeK {
-    
-    func solve() {
-        print(maximumSumSubArrayOfSizeK(array: [2, 3, 5, 2, 9, 7, 1], k: 3))
-    }
-    
-    private func maximumSumSubArrayOfSizeK(array: [Int], k: Int) -> Int {
-        var windowStart = 0
-        let windowSize = k
-        var sum = 0
-        var maxSum = Int.max
-        for windowEnd in 0..<array.count {
-            sum += array[windowEnd]
-            
-            if windowEnd - windowStart + 1 == windowSize {
-                maxSum = max(maxSum, sum)
-                sum -= array[windowStart]
-                windowStart += 1
-            }
+func maximumSumSubArrayOfSizeK(array: [Int], k: Int) -> Int {
+    var windowStart = 0
+    let windowSize = k
+    var sum = 0
+    var maxSum = Int.max
+    for windowEnd in 0..<array.count {
+        sum += array[windowEnd]
+        
+        if windowEnd - windowStart + 1 == windowSize {
+            maxSum = max(maxSum, sum)
+            sum -= array[windowStart]
+            windowStart += 1
         }
-       return maxSum
     }
+    return maxSum
 }
 
-let obj = MaximumSumSubArrayOfSizeK()
-print(obj.solve())
+print(maximumSumSubArrayOfSizeK(array: [2, 3, 5, 2, 9, 7, 1], k: 3))
+
